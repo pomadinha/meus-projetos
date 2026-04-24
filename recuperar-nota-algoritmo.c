5bf73f77b32a66642f745c7ab1127e119bf714a1
Kaio Lopes <136976042+pomadinha@users.noreply.github.com>
2026-04-24 20:31:21 -0300

    Set up main function and variable declarations
    Initialize variables for sales tracking.

--- recuperar-nota-algoritmo.c
+++ recuperar-nota-algoritmo.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(){
+
+  int quantVendas = 0;
+  float valor;
+  float totalVendido = 0.0;
+  float maiorVenda = 0.0;
+
+  printf("-------PAINEL-------");
+  
+  
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
