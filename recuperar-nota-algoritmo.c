747ab318c1d4e10fd8114f0274d07fda99403cfe
Kaio Lopes <136976042+pomadinha@users.noreply.github.com>
2026-04-24 21:09:10 -0300

    24-04-26

--- recuperar-nota-algoritmo.c
+++ recuperar-nota-algoritmo.c
@@ -4,21 +4,38 @@ int main(){
 
   int quantVendas = 0;
   float valor;
-  float totalVendido = 0.0;
+  float totalVendas = 0.0;
   float maiorVenda = 0.0;
 
-  printf("-------PAINEL-------");
+  printf("------- PAINEL DE PRESTAÇÃO DE CONTAS -------\n");
+  printf("Digite 0 para encerrar o programa.\n\n");
   
+  printf("Digite o valor da venda: R$");
+  scanf("%f", &valor);
   
-
-
-
-
-
-
-
-
-
-
-  return 0;
+  while(valor != 0){
+      if(valor < 0){
+          printf("\nValor inválido! Por favor, digite um valor válido.\n");
+      }else{
+          totalVendas += valor;
+          quantVendas++;
+          
+          if(valor > maiorVenda){
+              maiorVenda = valor;
+          }
+      }
+      
+      printf("Digite o valor da venda: R$");
+      scanf("%f",&valor);
+  }
+    printf("------- RELATÓRIO DE VENDAS -------\n");
+    
+    if(quantVendas > 0){
+        printf("Total vendido: %.2f\n", totalVendas);
+        printf("Quantidade de vendas realizadas: %.d\n", quantVendas);
+        printf("Maior valor de venda registrado: R$ %.2f\n", maiorVenda);
+    }else{
+        printf("Nenhuma venda foi registrada hoje.\n");
+    }
+    return 0;
 }
