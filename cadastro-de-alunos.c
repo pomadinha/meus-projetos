440ea98cc3f6aea1084f058befb610bb7927d84a
Kaio Lopes <136976042+pomadinha@users.noreply.github.com>
2026-04-27 21:44:18 -0300

    esboço

---
+++ cadastro-de-alunos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define QTD_NOTAS 3
+#define QTD_ALUNOS 30
+#define MEDIA 7.0
+
+void cadAlunos();
+void gerencNotas();
+void buscarAluno();
+
