0fb9111b66096ca9512a0d6e24a3b5935c411adc
Kaio Lopes <136976042+pomadinha@users.noreply.github.com>
2026-04-27 21:50:13 -0300

    Update sistema-aluno-notas.c

--- sistema-aluno-notas.c
+++ sistema-aluno-notas.c
@@ -4,7 +4,16 @@
 
 #define QTD_NOTAS 3
 #define QTD_ALUNOS 30
-#define MEDIA 7.0
+#define MEDIA_FINAL 7.0
+
+typedef struct{
+  int matricula; 
+  char situacao[15];
+  char nome[50]; 
+  char turma[20]; 
+  float notas [QTD_NOTAS]; 
+  float media;
+}Aluno;
 
 void cadAlunos();
 void gerencNotas();
